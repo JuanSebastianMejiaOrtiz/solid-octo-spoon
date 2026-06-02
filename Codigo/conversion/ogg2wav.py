7492a512910c69004e41b1df5513cd5260a70148
kagatto <jsmo012005@gmail.com>
2026-06-01 21:44:15 -0500

    NOW all audios are uploaded successfully

--- Codigo/conversion/ogg2wav.py
+++ Codigo/conversion/ogg2wav.py
@@ -2,8 +2,8 @@ from pydub import AudioSegment
 
 for genero in ["Hombre", "Mujer"]:
     for letra in ["a", "i", "u"]:
-        for i in range(1, 18 + 1):
-            if i == 18 and genero != "Hombre":
+        for i in range(1, 19 + 1):
+            if i == 18 or i == 19 and genero != "Hombre":
                 continue
             audio = AudioSegment.from_ogg(f"Audios/{genero}/{letra}_ogg/{letra}_{i}.ogg")
             audio.export(f"Audios/{genero}/{letra}/{letra}_{i}.wav", format="wav")
