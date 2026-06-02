958a19dc49c0dd3161cffa44d5228ec2bd594c55
kagatto <jsmo012005@gmail.com>
2026-06-01 21:07:10 -0500

    All audios uploaded :}

--- Codigo/conversion/ogg2wav.py
+++ Codigo/conversion/ogg2wav.py
@@ -2,6 +2,8 @@ from pydub import AudioSegment
 
 for genero in ["Hombre", "Mujer"]:
     for letra in ["a", "i", "u"]:
-        for i in range(1, 17 + 1):
-            audio = AudioSegment.from_ogg(f"{genero}/{letra}_ogg/{letra}_{i}.ogg")
-            audio.export(f"{genero}/{letra}/{letra}_{i}.wav", format="wav")
+        for i in range(1, 18 + 1):
+            if i == 18 and genero != "Hombre":
+                continue
+            audio = AudioSegment.from_ogg(f"Audios/{genero}/{letra}_ogg/{letra}_{i}.ogg")
+            audio.export(f"Audios/{genero}/{letra}/{letra}_{i}.wav", format="wav")
